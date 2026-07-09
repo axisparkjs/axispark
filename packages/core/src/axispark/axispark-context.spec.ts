131e970c98cc98fa4b37f75abfd0183172ec6859
Pablo Galvez <pablogalvez31@gmail.com>
2026-07-09 11:07:39 +0200

    feat: test mejorador para AxisparkContext

--- packages/core/src/axispark/axispark-context.spec.ts
+++ packages/core/src/axispark/axispark-context.spec.ts
@@ -1,16 +1,21 @@
-import { Logger } from '@axisparkjs/logger';
-import { Container } from '@axisparkjs/di';
+import {
+    ConsoleTransport,
+    Logger,
+    LogLevel,
+    SimpleFormatter
+} from '@axisparkjs/logger';
+import { Container, Injector } from '@axisparkjs/di';
 import { AxisparkContext } from './axispark-context';
 import { PluginRegistry } from '../plugin';
 
 describe('AxisparkContext', () => {
-    let context: AxisparkContext;
-
-    beforeEach(() => {
-        context = new AxisparkContext();
-    });
-
     describe('constructor', () => {
+        let context: AxisparkContext;
+
+        beforeEach(() => {
+            context = new AxisparkContext();
+        });
+
         it('should create a logger', () => {
             expect(context.logger).toBeInstanceOf(Logger);
         });
@@ -21,6 +26,52 @@ describe('AxisparkContext', () => {
 
         it('should create a DI container', () => {
             expect(context.container).toBeInstanceOf(Container);
+        });
+
+        it('should create an injector', () => {
+            expect(context.injector).toBeInstanceOf(Injector);
+        });
+
+        it('should create the default configuration', () => {
+            expect(context.config).toMatchObject({
+                name: 'App',
+                environment: 'development',
+                banner: true
+            });
+
+            expect(context.config.logTransports).toHaveLength(1);
+            expect(context.config.logTransports?.[0]).toBeInstanceOf(ConsoleTransport);
+        });
+
+        it('should register Logger in the container', () => {
+            expect(context.container.resolve(Logger)).toBe(context.logger);
+        });
+
+        it('should register Injector in the container', () => {
+            expect(context.container.resolve(Injector)).toBe(context.injector);
+        });
+
+        it('should override default configuration', () => {
+            const transport = new ConsoleTransport({
+                formatter: new SimpleFormatter(),
+                minLevel: LogLevel.ERROR
+            });
+
+            const context = new AxisparkContext({
+                name: 'MyApp',
+                environment: 'production',
+                banner: false,
+                logTransports: [transport]
+            });
+
+            expect(context.config).toEqual({
+                name: 'MyApp',
+                environment: 'production',
+                banner: false,
+                logTransports: [transport]
+            });
+
+            expect(context.logger).toBeInstanceOf(Logger);
         });
     });
 });
