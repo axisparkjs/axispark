96af854d34d11055efd4528e8ec1b106146551c7
Pablo31Monster <pablogalvez31@gmail.com>
2026-07-30 21:00:24 +0200

    feat: cambios en el sistema de pruebas

--- packages/engine/jest.config.ts
+++ packages/engine/jest.config.ts
@@ -3,12 +3,6 @@ import baseConfig from '../../jest.config.js';
 export const config = {
     ...baseConfig,
     collectCoverageFrom: ['src/**/*.{ts,js}'],
-    setupFiles: ['<rootDir>../../jest.setup.ts'],
-    moduleNameMapper: {
-        '^@axisparkjs/common$': '<rootDir>../../packages/common/src',
-        '^@axisparkjs/common/(.*)$': '<rootDir>../../packages/common/src/$1',
-        '^@axisparkjs/di$': '<rootDir>../../packages/di/src',
-        '^@axisparkjs/di/(.*)$': '<rootDir>../../packages/di/src/$1'
-    }
+    setupFiles: ['<rootDir>../../jest.setup.ts']
 };
 export default config;
--- packages/http/jest.config.ts
+++ packages/http/jest.config.ts
@@ -3,10 +3,6 @@ import baseConfig from '../../jest.config.js';
 export const config = {
     ...baseConfig,
     collectCoverageFrom: ['src/**/*.{ts,js}'],
-    setupFiles: ['<rootDir>../../jest.setup.ts'],
-    moduleNameMapper: {
-        '^@axisparkjs/common$': '<rootDir>../../packages/common/src',
-        '^@axisparkjs/common/(.*)$': '<rootDir>../../packages/common/src/$1'
-    }
+    setupFiles: ['<rootDir>../../jest.setup.ts']
 };
 export default config;
