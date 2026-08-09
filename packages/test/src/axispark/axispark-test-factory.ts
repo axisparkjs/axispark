b668a690f42f4c955537b645cdadc2505f3b2f4b
Pablo Galvez <pablogalvez31@gmail.com>
2026-08-09 17:23:57 +0200

    revert: 76227b187a0d2e4a545299622247bfd927928568 & added NODE_OPTIONS=--experimental-vm-modules to jest command for e2e tests

--- packages/test/src/axispark/axispark-test-factory.ts
+++ packages/test/src/axispark/axispark-test-factory.ts
@@ -1,7 +1,7 @@
 import { Factory } from '@axisparkjs/common';
 import { AxiSparkTestConfig } from './axispark-test-config';
 import { AxiSparkContext, AxiSparkCore } from '@axisparkjs/core';
-import { ConsoleTransport, LogLevel, SimpleFormatter } from '@axisparkjs/logger';
+import { NullTransport, LogLevel, SimpleFormatter } from '@axisparkjs/logger';
 
 class AxiSparkTestFactoryStatic implements Factory<AxiSparkCore> {
     public create(config?: AxiSparkTestConfig): AxiSparkCore {
@@ -12,7 +12,7 @@ class AxiSparkTestFactoryStatic implements Factory<AxiSparkCore> {
                 banner: false,
                 environment: 'test',
                 name: 'Test',
-                logTransports: [new ConsoleTransport({ formatter: new SimpleFormatter(), minLevel: LogLevel.Error })]
+                logTransports: [new NullTransport({ formatter: new SimpleFormatter(), minLevel: LogLevel.Error })]
             },
             {
                 scanner: 'file-system',
